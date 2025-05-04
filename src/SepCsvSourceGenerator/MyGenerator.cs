c80c8bf83333c571877dce7b4ba32038aa96c2a7
Austin Wise <AustinWise@gmail.com>
2025-05-04 13:31:55 -0700

    Basic analyzer project.

---
+++ src/SepCsvSourceGenerator/MyGenerator.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace SepCsvSourceGenerator
+{
+    [Generator]
+    internal class MyGenerator : IIncrementalGenerator
+    {
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+        }
+    }
+}
