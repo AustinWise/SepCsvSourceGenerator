df7df9eeb1502bf91beb7d92b2f02d1c7977e616
Austin Wise <AustinWise@gmail.com>
2025-07-04 15:49:46 -0700

    Remove spurious copyright statement.
    This does not appear similar to any of the RoslynTestUtils classes found
    on grep.app.

--- tests/SepCsvSourceGenerator.Analyzer.Tests/RoslynTestUtils.cs
+++ tests/SepCsvSourceGenerator.Analyzer.Tests/RoslynTestUtils.cs
@@ -1,11 +1,7 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
-using System.Collections.Immutable;
-using System.Reflection;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+using System.Reflection;
 
 namespace SepCsvSourceGenerator.Analyzer.Tests
 {
