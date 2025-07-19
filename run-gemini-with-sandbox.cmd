0cc94df9b1f8d08c268132465fc88506250774dd
Austin Wise <AustinWise@gmail.com>
2025-07-19 13:03:17 -0700

    Clarify naming

--- run-gemini-with-sandbox.cmd
+++ run-gemini-with-sandbox.cmd
@@ -2,7 +2,7 @@
 setlocal
 cd /d %~dp0
 
-REM Use .gemini\build.cmd to build this image.
+REM Use .gemini\build-docker.cmd to build this image.
 REM Currently we use this Docker sandbox because Gemini CLI does not handle Windows command execution
 REM very well.
 gemini -s --sandbox-image sep-csv-source-generator-gemini-sandbox
